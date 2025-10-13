73c0031821ac8941a82055b9a298dc58ad0bed0b
karbonpanzer <74460271+karbonpanzer@users.noreply.github.com>
2025-10-13 11:11:52 -0400

    Removed redundancies

--- Source/MoteDualAttachedForAlbum.cs
+++
@@ -1,9 +0,0 @@
-using Verse;
-
-namespace VanillaMusicExpanded
-{
-    public class MoteDualAttachedForAlbum : MoteDualAttached
-    {
-        // Simple subclass to avoid depending on the Books version
-    }
-}
