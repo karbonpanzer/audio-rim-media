fff2a55e48d07d6656a09835488b1f1a4c0e9b05
karbonpanzer <74460271+karbonpanzer@users.noreply.github.com>
2025-10-14 23:19:16 -0400

    8

--- Source/ModInit.cs
+++ Source/ModInit.cs
@@ -1,9 +1,13 @@
+using HarmonyLib;
 using Verse;
 
 namespace VanillaMusicExpanded
 {
     public class VME_Mod : Mod
     {
-        public VME_Mod(ModContentPack content) : base(content) { }
+        public VME_Mod(ModContentPack content) : base(content)
+        {
+            new Harmony("kp.rimradio").PatchAll();
+        }
     }
 }
